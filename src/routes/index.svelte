0418657ba2e06f023a3496af2fafc418c9cb153c
santoyox714 <santoyox714@gmail.com>
2022-06-02 17:09:05 -0700

    index.svelte updated

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<!-- src/routes/index.svelte -->
+<svelte:head>
+ <title>Angel Santoyo</title>
+</svelte:head>
+
+<div class="container">
+ <div class="info">
+   <div class="animate-background">
+     <div class="container info-container">
+     <div class="info-text">
+       <h1>
+        <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" >
+       <p class="animate-text-1">Major:Computer Science</p>
+       <p class="text-2">California State University, Fullerton</p>
+       </h1>
+     </div>
+     <h2 class="subtitle">
+       First generation student with a passion for learning new concepts regularly.
+     </h2>
+     <div style="color:black" class="scroll-button animate-scroll" tabindex="0">
+       <a href="/about">Click Here</a><div class="arrow">→</div>
+     </div>
+     </div>
+   </div>
+ </div>
+</div>
+
+<div class="background-div"></div>
+
+
