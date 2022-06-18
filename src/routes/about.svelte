68647d2cb1fdcd585c1f6d1e291f4fdc42c14eed
santoyox714 <santoyox714@gmail.com>
2022-06-17 21:26:38 -0700

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,7 +9,7 @@
          <div class = "header-image"></div>
          <div class="about-subtitle">
              <p>
-            I'm a first generation student majoring in computer science at CSU Fullerton. 
+            I'm a first generation student majoring in computer science at California State University, Fullerton. 
             I'm very passionate about learning new concepts regularly, specifically about web development, game development/design, as well as software engineering. Outside of school I like to play video games, learn about cars, and try new foods.
              </p>
              
