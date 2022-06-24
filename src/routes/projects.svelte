a3c1b45c4a1d92e6b359fc999df64371773f14c0
santoyox714 <santoyox714@gmail.com>
2022-06-23 20:05:18 -0700

    Update projects.svelte

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -6,26 +6,40 @@
  
  <div class="projects">
     <div class="project-title "> Projects </div> 
-    <div class="project-1"></div>
+    <div class="project-1">project1 test </div>
           
  </div>
 
 
  <div class="experience">
-    <div class="exp-1"></div>
-   
+    <div class="exp-title"> Experience</div>
+    <div class="exp-1">exp1 test </div>
        
  </div>
 
 
 <style>
     .project-title{
+      font-size: 3.0rem;
+      font-weight: 650;
+       margin-top: 2rem;
+    }
+
+  .project-1 {
+    width:fit-content;
+    height:fit-content;
+    border:1px solid #ffffff;
+    background:rgb(25, 67, 122);
+    position:center;
+    border-radius: 10px;
+}
+.exp-title{
         font-size: 3.0rem;
        font-weight: 650;
        margin-top: 2rem;
     }
 
-  .project-1 {
+  .exp-1 {
     width:fit-content;
     height:fit-content;
     border:1px solid #ffffff;
