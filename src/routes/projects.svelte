b072891f9485058e5c0c116dc109c5b9df2762a1
santoyox714 <santoyox714@gmail.com>
2022-06-24 17:20:14 -0700

    Update projects.svelte

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,50 +1,13 @@
-
-<svelte:head>
-    <title>Experience</title>
- </svelte:head>
- 
- 
- <div class="projects">
-    <div class="project-title "> Projects </div> 
-    <div class="project-1">project1 test </div>
-          
- </div>
-
-
- <div class="experience">
-    <div class="exp-title"> Experience</div>
-    <div class="exp-1">exp1 test </div>
-       
- </div>
-
-
-<style>
-    .project-title{
-      font-size: 3.0rem;
-      font-weight: 650;
-       margin-top: 2rem;
-    }
-
-  .project-1 {
-    width:fit-content;
-    height:fit-content;
-    border:1px solid #ffffff;
-    background:rgb(25, 67, 122);
-    position:center;
-    border-radius: 10px;
-}
-.exp-title{
-        font-size: 3.0rem;
-       font-weight: 650;
-       margin-top: 2rem;
-    }
-
-  .exp-1 {
-    width:fit-content;
-    height:fit-content;
-    border:1px solid #ffffff;
-    background:rgb(25, 67, 122);
-    position:center;
-    border-radius: 10px;
-}
-</style>
+<script>
+	import Item from './accordian.svelte'
+	const data = {
+		'Projects': 
+      ["info here"],
+      
+		'Experience': ["info here"],
+		
+	}
+</script>
+{#each Object.entries(data) as entry}
+<Item {entry} />
+{/each}
